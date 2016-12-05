2859ae71a951dcadb2b1d205db6db52f6efec40c
Ariel Diamond <diamond@jellyvision.com>
2016-12-04 20:01:30 -0600

    merge from master

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,8 @@ export const FETCH_QUESTIONS = 'FETCH_QUESTIONS';
 export function fetchQuestions() {
 	// TODO: figure out why this const isn't working
 	// const url = `${API_URL}/questions`;
-	// const url = 'http://localhost:3000/api/v1/questions';
-	const url = 'http://0.0.0.0:3000/api/v1/questions';
+	const url = 'http://localhost:3000/api/v1/questions';
+	// const url = 'http://0.0.0.0:3000/api/v1/questions';
 	const request = axios.get(url);
 	return {
 		type: FETCH_QUESTIONS,
@@ -18,6 +18,7 @@ export function fetchQuestions() {
 export const GET_NEXT_QUESTION = 'GET_NEXT_QUESTION';
 export function getNextQuestion(current, history) {
 	const url = 'http://localhost:3000/api/v1/next_question';
+	// const url = 'http://0.0.0.0:3000/api/v1/next_question';
 	const request = axios.post(url, { current, history });
 	return {
 		type: GET_NEXT_QUESTION,
